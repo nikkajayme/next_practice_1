f02c0233e1d155acc814288f0746fc1e067016a8
Nikka Acuña <njacuna1611@gmail.com>
2023-02-13 11:57:24 +0800

    first-commit

---
+++ app/practice_1/components/ArticleList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ArticleItem from './ArticleItem';
+import articleStyles from '../styles/Article.module.css';
+
+const ArticleList = ({ articles }) => {
+    return (
+        <div className={articleStyles.grid}>
+            {articles.map((article) => (
+                <ArticleItem
+                    article={article}
+                    key={article.id}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default ArticleList;
